31d684920aa643859d82cd50109a973a346770b5
YaroslavBorshevsky812 <Speransky812@mail.ru>
2022-03-27 13:02:53 +0300

    Inner join [#373617 #279338]

---
+++ src/sql/inner_join.sql
@@ -0,0 +1,32 @@
+create table users (
+    id serial primary key,
+    name varchar(255),
+    age int
+);
+
+create table pets (
+    id serial primary key,
+    name varchar(255),
+    species varchar(255),
+    owner int references users(id) unique
+);
+
+insert into users(name, age) values('Yaroslav', 25);
+insert into users(name, age) values('Mark', 35);
+insert into users(name, age) values('Ann', 26);
+
+
+insert into pets(species, owner) values('Собака', 1);
+insert into pets(species, owner) values('Кошка', 2);
+insert into pets(species, owner) values('Белка', 3);
+
+update pets set name='Альма' where species = 'Собака';
+update pets set name='Тимофей' where species = 'Кошка';
+update pets set name='Бельчиха' where species = 'Белка';
+
+insert into pets(species, name, owner) values('Дельфин', 'Сплинтер', 3);
+
+select * from pets
+join users u on pets.owner = u.id;
+
+select id as Идентификатор, name as Имя from users;
